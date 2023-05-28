9309075ebf7b1582200963ce313052c66eac9195
Vedant <vedantnp200@gmail.com>
2023-05-28 22:33:13 +0530

    Single Inheritance Program is added

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Object_Oriented_Programming_Inheritance
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Single Inheritance
+
+            Employee emp = new Employee(111, 50000, "Jack Ryan", 30);
+
+            emp.DisplayInfo();
+
+            // Multilevel Inheritance
+
+
+        }
+    }
+}
---
+++ SingleInheritance.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Object_Oriented_Programming_Inheritance
+{
+
+    // Single Inheritance: A derived class that inherits from only one base class.
+    internal class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+    internal class Employee: Person
+    {
+        public int emp_id;
+        public int emp_salary;
+
+        public Employee(int id, int salary, string name, int age) : base(name, age)
+        {
+            this.emp_id = id;
+            this.emp_salary = salary;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Employee ID: " + emp_id);
+            Console.WriteLine("Employee Name: " + name);
+            Console.WriteLine("Employee Age: " + age);
+            Console.WriteLine("Employee Salary: " + emp_salary);
+        }
+    }
+}

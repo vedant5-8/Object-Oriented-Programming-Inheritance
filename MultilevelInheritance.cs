8feab6926bd309e84e60f4319cf682e768301e2b
Vedant <vedantnp200@gmail.com>
2023-05-28 23:12:16 +0530

    Hierarchical Inheritance code is added

---
+++ MultilevelInheritance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Object_Oriented_Programming_Inheritance
+{
+    internal class BOI
+    {
+        public double rate_boi = 6.5;
+
+        public double CalculateBOIBalance(int amount, int year)
+        {
+            return amount + ((amount * rate_boi * year) / 100);
+        }
+    }
+    internal class Union:BOI
+    {
+        public double rate_union = 7.0;
+
+        public double CalculateUnionBalance(int amount, int year)
+        {
+            return amount + ((amount * rate_union * year) / 100);
+        }
+    }
+    internal class SBI: Union
+    {
+        public double rate_sbi = 7.5;
+
+        public double CalculateSBIBalance(int amount, int year)
+        {
+            return amount + ((amount * rate_sbi * year) / 100);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,13 +8,27 @@ namespace Object_Oriented_Programming_Inheritance
         {
             // Single Inheritance
 
+            Console.WriteLine("-----Single Inheritance-----");
             Employee emp = new Employee(111, 50000, "Jack Ryan", 30);
 
             emp.DisplayInfo();
 
             // Multilevel Inheritance
 
+            Console.WriteLine("\n-----Multilevel Inheritance-----");
+            SBI sbi = new SBI();
 
+            Console.WriteLine("Enter the amount you want to invest?");
+            int amount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("How many time you want to invest (in years)?");
+            int years = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your amount according to Bank of India policies after {0} years will be {1}.", years, sbi.CalculateBOIBalance(amount, years));
+            Console.WriteLine("Your amount according to Union Bank of India policies after {0} years will be {1}.", years, sbi.CalculateUnionBalance(amount, years));
+            Console.WriteLine("Your amount according to State Bank of India policies after {0} years will be {1}.", years, sbi.CalculateSBIBalance(amount, years));
+
+            
         }
     }
 }
